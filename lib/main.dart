e5b82157ffdf87330e9536b3de2d4ef9d0a5b322
603-16\kccistc <kimhyeanseo070709@gmail.com>
2024-07-29 17:51:34 +0900

    end

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'stack.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+  return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -22,40 +22,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("안녕하세요"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            ListTile(
-              title: Text("Menu 1"),
-            ),
-            DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text("Drawer Header Part"))
-          ],
-        ),
-      ),
-      body: const Center(child: Text("HELLO WORLD")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("click click"),
-        child: const Icon(Icons.mouse),
-      ),
-    );
-  }
-}
---
+++ lib/stack.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
+        Container(
+          width: 90,
+          height: 90,
+          color: Colors.green,
+        ),
+        Container(
+          width: 80,
+          height: 80,
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
