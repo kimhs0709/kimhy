d8d71efdd9fb7e70c52a32baf55ebed6b409337c
603-16\kccistc <kimhyeanseo070709@gmail.com>
2024-07-29 14:36:57 +0900

    comic

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("안녕하세요"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            ListTile(
+              title: Text("Menu 1"),
+            ),
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text("Drawer Header Part"))
+          ],
+        ),
+      ),
+      body: const Center(child: Text("HELLO WORLD")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("click click"),
+        child: const Icon(Icons.mouse),
+      ),
+    );
+  }
+}
